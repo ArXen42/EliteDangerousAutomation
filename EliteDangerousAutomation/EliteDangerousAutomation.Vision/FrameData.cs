3aaef96c922be9fd134a3e76dc75455611c99bd0
Georgii Pelageikin <arxen42@tutanota.com>
2018-04-12 01:21:26 +0300

    FrameData interface

--- EliteDangerousAutomation/EliteDangerousAutomation.Vision/FrameData.cs
+++ EliteDangerousAutomation/EliteDangerousAutomation.Vision/FrameData.cs
@@ -1,11 +1,29 @@
 ﻿using System.Drawing;
 
-namespace EliteDangerousAutomation.Vision {
+namespace EliteDangerousAutomation.Vision
+{
 	/// <summary>
 	///     Contains data retrieved from frame.
 	/// </summary>
 	internal struct FrameData
 	{
-		public FrameData(Bitmap frame) { }
+		/// <summary>
+		///     Perform frame analysis and puts result into provided <see cref="destinationFrameData"/>.
+		/// </summary>
+		/// <param name="frame">Frame bitmap.</param>
+		/// <param name="destinationFrameData">Destination frame data.</param>
+		public static void FromFrameBitmap(Bitmap frame, ref FrameData destinationFrameData) { }
+
+		/// <summary>
+		///     Perform frame analysis and returns resulting frame data.
+		/// </summary>
+		/// <param name="frame">Frame bitmap.</param>
+		public static FrameData FromFrameBitmap(Bitmap frame)
+		{
+			var data = new FrameData();
+			FromFrameBitmap(frame, ref data);
+
+			return data;
+		}
 	}
 }
